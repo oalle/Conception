3b9f51878cd9d40ce62477ff2373d6731a7e23f1
AngelineBrocq <38132631+AngelineBrocq@users.noreply.github.com>
2019-10-28 20:19:51 +0100

    Affichage des coeurs

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject m_HeartsComponent;
+    private int m_Life = 3;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (m_Life != 0)
+            {
+
+                m_Life--;
+                int l_HeartsCount = m_HeartsComponent.transform.childCount;
+                print("hearts = " + l_HeartsCount);
+                for (int i = 0; i < l_HeartsCount - m_Life; i++)
+                {
+                    print(i);
+                    m_HeartsComponent.transform.GetChild(i).gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
